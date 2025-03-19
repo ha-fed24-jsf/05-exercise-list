887aac5f88d9b639dba221ce947a09a14e2454e7
David Andersson <lejonmanen@gmail.com>
2025-03-19 09:15:18 +0100

    Initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+
+const App = () => (
+	<div className="column">
+		<header>
+			header
+		</header>
+		<main>
+			main
+		</main>
+	</div>
+)
+
+export default App
